479573950510f466254306ec144c97a2a1d07701
scj100586 <scj100586@users.noreply.github.com>
2015-07-14 16:07:58 +0800

    Create a.c

---
+++ a.c
@@ -0,0 +1,254 @@
+f1413d5e0cfa0450436d052e6efa1324
+
+totallen: 99997532
+BlockIndex: 99997532
+Block0 upload ok!
+Time: 1.04170107841
+totallen: 99997532
+BlockIndex: 99997532
+Block1 upload ok!
+Time: 1.0371940136
+totallen: 99997532
+BlockIndex: 99997532
+Block2 upload ok!
+Time: 1.73401713371
+totallen: 99997532
+BlockIndex: 99997532
+Block3 upload ok!
+Time: 1.02847099304
+totallen: 99997532
+BlockIndex: 99997532
+Block4 upload ok!
+Time: 303.182303905
+totallen: 99997532
+BlockIndex: 99997532
+Block5 upload ok!
+Time: 328.917577028
+totallen: 99997532
+BlockIndex: 99997532
+Block6 upload ok!
+Time: 336.870259047
+totallen: 99997532
+BlockIndex: 99997532
+Block7 upload ok!
+Time: 338.447818995
+
+
+
+r.set:
+100Times: 0.0117199420929
+1000Times: 0.0764429569244
+10000Times: 0.747045993805
+100000Times: 6.80693697929
+1000000Times: 70.1394121647
+rc.set:
+100Times: 0.054719209671
+1000Times: 0.545240163803
+10000Times: 5.81196904182
+100000Times: 58.2394959927
+1000000Times: 589.974274158
+
+r.hset:
+100Times: 0.012757062912
+1000Times: 0.0827081203461
+10000Times: 0.741104841232
+100000Times: 7.57543802261
+1000000Times: 82.6375498772
+rc.hset:
+100Times: 0.062096118927
+1000Times: 0.580042123795
+10000Times: 5.89770102501
+100000Times: 59.2279880047
+1000000Times: 594.722062111
+
+first 100M/Every
+rgb/part/part1.txt's time: 8.53337001801
+/home/rgb/part/part9.txt
+rgb/part/part9.txt's time: 8.5624229908
+/home/rgb/part/part4.txt
+rgb/part/part4.txt's time: 0.627895116806
+/home/rgb/part/part7.txt
+rgb/part/part7.txt's time: 8.54158592224
+/home/rgb/part/part3.txt
+rgb/part/part3.txt's time: 0.638121128082
+/home/rgb/part/part5.txt
+rgb/part/part5.txt's time: 8.2114739418
+/home/rgb/part/part8.txt
+rgb/part/part8.txt's time: 0.615993976593
+/home/rgb/part/part6.txt
+rgb/part/part6.txt's time: 8.50942802429
+/home/rgb/part/part2.txt
+rgb/part/part2.txt's time: 8.21724390984
+
+second 100M/Every
+rgb/part/part1.txt's time: 0.311146020889
+/home/rgb/part/part9.txt
+rgb/part/part9.txt's time: 0.311667203903
+/home/rgb/part/part4.txt
+rgb/part/part4.txt's time: 0.315714120865
+/home/rgb/part/part7.txt
+rgb/part/part7.txt's time: 0.316180944443
+/home/rgb/part/part3.txt
+rgb/part/part3.txt's time: 0.307754039764
+/home/rgb/part/part5.txt
+rgb/part/part5.txt's time: 0.306970119476
+/home/rgb/part/part8.txt
+rgb/part/part8.txt's time: 0.309917926788
+/home/rgb/part/part6.txt
+rgb/part/part6.txt's time: 0.311499118805
+/home/rgb/part/part2.txt
+rgb/part/part2.txt's time: 0.310655117035
+
+first 1~4/200M 5~9/100M
+rgb/part/part1.txt's time: 16.6968069077
+/home/rgb/part/part9.txt
+rgb/part/part9.txt's time: 8.56564211845
+/home/rgb/part/part4.txt
+rgb/part/part4.txt's time: 14.8839991093
+/home/rgb/part/part7.txt
+rgb/part/part7.txt's time: 8.50804185867
+/home/rgb/part/part3.txt
+rgb/part/part3.txt's time: 15.41188097
+/home/rgb/part/part5.txt
+rgb/part/part5.txt's time: 8.2308909893
+/home/rgb/part/part8.txt
+rgb/part/part8.txt's time: 0.61638712883
+/home/rgb/part/part6.txt
+rgb/part/part6.txt's time: 8.5001718998
+/home/rgb/part/part2.txt
+rgb/part/part2.txt's time: 5.50311994553
+
+
+first 1~4/200M 5~9/100M
+rgb/part/part1.txt's time: 0.606505870819
+/home/rgb/part/part9.txt
+rgb/part/part9.txt's time: 0.315417051315
+/home/rgb/part/part4.txt
+rgb/part/part4.txt's time: 0.520122051239
+/home/rgb/part/part7.txt
+rgb/part/part7.txt's time: 0.317062139511
+/home/rgb/part/part3.txt
+rgb/part/part3.txt's time: 0.536321878433
+/home/rgb/part/part5.txt
+rgb/part/part5.txt's time: 0.313616991043
+/home/rgb/part/part8.txt
+rgb/part/part8.txt's time: 0.31174993515
+/home/rgb/part/part6.txt
+rgb/part/part6.txt's time: 0.310172080994
+/home/rgb/part/part2.txt
+rgb/part/part2.txt's time: 0.745306015015
+
+**************************************
+packages !!
+
+totallen: 132405571
+BlockIndex: 132405571
+Block0 upload ok!
+Time: 12.3628840446
+ 
+totallen: 266304354
+BlockIndex: 266304354
+Block1 upload ok!
+Time: 26.5740430355
+ 
+totallen: 136676192
+BlockIndex: 136676192
+Block2 upload ok!
+Time: 1.29458093643
+ 
+totallen: 246840133
+BlockIndex: 246840133
+Block3 upload ok!
+Time: 6.70159983635
+ 
+totallen: 146684488
+BlockIndex: 146684488
+Block4 upload ok!
+Time: 13.4305679798
+ 
+totallen: 40149734
+BlockIndex: 40149734
+Block5 upload ok!
+Time: 3.79770708084
+ 
+totallen: 141879535
+BlockIndex: 141879535
+Block6 upload ok!
+Time: 1.00342202187
+ 
+totallen: 121079010
+BlockIndex: 121079010
+Block7 upload ok!
+Time: 1.02662110329
+ 
+totallen: 149018823
+BlockIndex: 149018823
+Block8 upload ok!
+Time: 13.738724947
+ 
+totallen: 149921413
+BlockIndex: 149921413
+Block9 upload ok!
+Time: 14.67674613
+ 
+totallen: 126926960
+BlockIndex: 126926960
+Block10 upload ok!
+Time: 2.54527711868
+ 
+put into redis cluster time: 97.8001558781
+
+*************************************************
+put block from disk packagespart first time
+
+/home/rgb/packagespart/2
+rgb/packagespart/2's time: 12.4368810654
+/home/rgb/packagespart/8
+rgb/packagespart/8's time: 1.02022504807
+/home/rgb/packagespart/0
+rgb/packagespart/0's time: 12.7186830044
+/home/rgb/packagespart/7
+rgb/packagespart/7's time: 0.858464956284
+/home/rgb/packagespart/3
+rgb/packagespart/3's time: 24.4938127995
+/home/rgb/packagespart/6
+rgb/packagespart/6's time: 13.0626559258
+/home/rgb/packagespart/9
+rgb/packagespart/9's time: 14.4999289513
+/home/rgb/packagespart/1
+rgb/packagespart/1's time: 25.0210430622
+/home/rgb/packagespart/4
+rgb/packagespart/4's time: 14.4445860386
+/home/rgb/packagespart/10
+rgb/packagespart/10's time: 11.5824398994
+/home/rgb/packagespart/5
+rgb/packagespart/5's time: 3.70038795471
+put into redis cluster time: 133.845332146
+
+put block from disk packagespart second time
+list get
+/home/rgb/packagespart/2
+rgb/packagespart/2's time: 0.463337898254
+/home/rgb/packagespart/8
+rgb/packagespart/8's time: 0.514626026154
+/home/rgb/packagespart/0
+rgb/packagespart/0's time: 0.450516939163
+/home/rgb/packagespart/7
+rgb/packagespart/7's time: 0.416751146317
+/home/rgb/packagespart/3
+rgb/packagespart/3's time: 0.835793972015
+/home/rgb/packagespart/6
+rgb/packagespart/6's time: 0.496840953827
+/home/rgb/packagespart/9
+rgb/packagespart/9's time: 0.5095038414
+/home/rgb/packagespart/1
+rgb/packagespart/1's time: 0.894288063049
+/home/rgb/packagespart/4
+rgb/packagespart/4's time: 0.512501001358
+/home/rgb/packagespart/10
+rgb/packagespart/10's time: 0.438509941101
+/home/rgb/packagespart/5
+rgb/packagespart/5's time: 0.147100925446
+put into redis cluster time: 5.68333888054
+
+
